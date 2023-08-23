923f84858213f94b13ce6dcf4117b89b02c529e5
lamb0693 <mar189@naver.com>
2023-08-24 07:21:35 +0900

    modify modify.js

--- src/main/resources/static/js/modifyInfo.js
+++ src/main/resources/static/js/modifyInfo.js
@@ -14,3 +14,30 @@ const withdraw = (event) => {
     }
 };
 btnWithdraw.addEventListener("click", withdraw);
+const btnChangePassword = document.getElementById("btnChangePassword");
+if (btnChangePassword == null)
+    console.log("error --- btnChangePassword is null");
+const changePassword = (event) => {
+    const txtPassword1 = document.getElementById("txtPassword1");
+    if (txtPassword1 == null)
+        console.log("error --- txtPassword1 is null");
+    const txtPassword2 = document.getElementById("txtPassword2");
+    if (txtPassword2 == null)
+        console.log("error --- txtPassword2 is null");
+    const changePasswordForm = document.getElementById("changePasswordForm");
+    if (changePasswordForm == null)
+        alert("error --- changePasswordForm is null");
+    if (txtPassword1.value.length < 8) {
+        alert("패스워드1 : 8자리 이상이 필요합니다 ");
+        return;
+    }
+    else if (txtPassword2.value.length < 8) {
+        alert("패스워드2 : 8자리 이상이 필요합니다 ");
+    }
+    else if (txtPassword1.value != txtPassword2.value) {
+        alert("두 개의 패스워드가 일치하지 않습니다");
+        return;
+    }
+    changePasswordForm.submit();
+};
+btnChangePassword.addEventListener("click", changePassword);
