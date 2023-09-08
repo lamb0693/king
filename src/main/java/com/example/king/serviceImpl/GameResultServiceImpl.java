9cb5c7b5665ad2b6ecf8847a02bea6079cf0a53b
lamb0693 <mar189@naver.com>
2023-09-08 16:29:03 +0900

    delete comment

--- src/main/java/com/example/king/serviceImpl/GameResultServiceImpl.java
+++ src/main/java/com/example/king/serviceImpl/GameResultServiceImpl.java
@@ -26,25 +26,6 @@ public class GameResultServiceImpl implements GameResultService {
 
     private GameResultRepository gameResultRepository;
 
-//    @Override
-//    public List<GameResultListDTO> getGameResultList() {
-//        List<GameResultEntity> entityList = gameResultRepository.findAll();
-//        List<GameResultListDTO> dtoList = new ArrayList<>();
-//
-//        for(GameResultEntity entity : entityList){
-//            GameResultListDTO dto = new GameResultListDTO();
-//            dto.setGame_id( entity.getGame_id());
-//            dto.setGame_kind( entity.getGameKind().name());
-//            dto.setWinner_id( entity.getWinner().getId());
-//            dto.setLoser_id( entity.getLoser().getId());
-//            dto.setGamedate( entity.getGamedate());
-//
-//            dtoList.add(dto);
-//        }
-//
-//        return dtoList;
-//    }
-
     private List<GameResultListDTO> entityToDTO( List<GameResultEntity> entityList){
         List<GameResultListDTO> dtoList = new ArrayList<>();
 
