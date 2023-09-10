80ac1c462c0652adb746b23abdad2af7f1ddf988
lamb0693 <mar189@naver.com>
2023-09-10 21:12:56 +0900

    modify securityConfig

--- src/main/java/com/example/king/config/SecurityConfig.java
+++ src/main/java/com/example/king/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -62,6 +63,9 @@ public class SecurityConfig {
                     .anyRequest().authenticated();
         })
         .userDetailsService(memberUserDetailsService)
+        .httpBasic(
+                AbstractHttpConfigurer::disable
+        )
         .formLogin( (form) -> {
             form.loginPage("/auth/login") //PostMappeing("/auth/login")으로 login 설정
                 .usernameParameter("id") // loadByUserName 의 parameter를 지정 default=username
